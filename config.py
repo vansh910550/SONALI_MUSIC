13068b1830080ccb6a5840ff090c3852201864f1
vansh910550 <vansh910550@gmail.com>
2025-06-24 22:24:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_HASH = getenv("c3632bb97809c2e824ee595d638e3d02")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("7642171843:AAECY2-V4PGCTtNRBCJ4ASlpoiIrBzFtbw0")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","ll_ALPHA_BABY_lll")
+OWNER_USERNAME = getenv("@Mr_Qtuu_panda","ll_ALPHA_BABY_lll")
 # --------------------------------------------------------
 BOT_USERNAME = getenv("BOT_USERNAME","Sonali_Music_bot")
 # --------------------------------------------------------
@@ -48,8 +48,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PURVI_SUPPORT")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PURVI_UPDATES")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Thediaryofemotions")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+yCuRL5OmvFs1YjY1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
