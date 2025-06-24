51d7fe1daf03b325d661af105e1ac93b14a742e5
vansh910550 <vansh910550@gmail.com>
2025-06-25 00:44:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("27971375"))
-API_HASH = getenv("c3632bb97809c2e824ee595d638e3d02")
+API_ID = int(getenv("API_ID")
+API_HASH = getenv("API_HASH")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("7642171843:AAECY2-V4PGCTtNRBCJ4ASlpoiIrBzFtbw0")
+BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
 OWNER_USERNAME = getenv("OWNER_USERNAME","@Mr_Qtuu_panda")
 # --------------------------------------------------------
